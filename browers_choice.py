d78400cb3ff164a457da02db321f5558e76ed3dd
Josh Laptop <josh.warburton@yahoo.com>
2023-09-06 02:25:35 +0100

    Added Edge

--- browers_choice.py
+++ browers_choice.py
@@ -3,22 +3,26 @@ from selenium.webdriver.firefox.service import Service as FirefoxService
 from webdriver_manager.firefox import GeckoDriverManager
 from selenium.webdriver.chrome.service import Service as ChromeService
 from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.edge.service import Service as EdgeService
+from webdriver_manager.microsoft import EdgeChromiumDriverManager
 
 def initialize_driver(browser_choice):
     if browser_choice == 1:
         # Firefox
         options = webdriver.FirefoxOptions()
-        #options.add_argument('-host=127.0.0.1')
         options.headless = True
-        # Start the Firefox browser with the provided options
         print("Starting Firefox")
         driver = webdriver.Firefox(service=FirefoxService(GeckoDriverManager().install()), options=options)
     elif browser_choice == 2:
         # Chrome
-        options = webdriver.ChromeOptions()    
-        options.add_argument('--headless=new') # Run the browser in headless mode (without opening a visible browser window)
+        options = webdriver.ChromeOptions()
+        options.add_argument('--headless=new')
         driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()), options=options)
-    else:
-        print("Invalid browser choice. Exiting...")
-        exit(1) # Exit the program if the user enters an invalid choice   
+    elif browser_choice == 3:
+        # Microsoft Edge
+        options = webdriver.EdgeOptions()
+        options.use_chromium = True
+        options.add_argument ("headless")
+        driver = webdriver.Edge(service=EdgeService(EdgeChromiumDriverManager().install()), options=options)
+        print ("Starting Edge\nCurrently undergoing improvements, including resolving errors and enhancing compatibility with certain websites.")
     return driver
